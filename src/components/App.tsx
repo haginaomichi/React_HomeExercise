4fbf2805b7ab8cf3b690ca9358d10024d2374f32
haginaomichi <blue-earth1996@docomo.ne.jp>
2022-03-27 23:40:37 +0900

    issue/4　No不一致エラー解消

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ export const App: FC = () => {
     const [number, setNumber] = useState<string>("");
     const [menus, setMenus] = useState<string[]>([]);
     const [Error, setError] = useState("");
+    const [check, setCheck] = useState("");
     const [kcals, setKcals] = useState<string[]>([]);
     const [foods, setFoods] = useState<string[]>([]);
     const [hide, setHide] = useState('1');
@@ -18,6 +19,7 @@ export const App: FC = () => {
         newMenus.splice(0, 3);
       };
       setError("");
+      setCheck(number);
       setKcals([]);
       setFoods([]);
       setHide("");
@@ -42,43 +44,48 @@ export const App: FC = () => {
     };
 
     const onClickComplete = () =>{
-      const newKcals = [...kcals];
-      if (kcals !== []){
-        newKcals.splice(0, 3);
-      };
-      const newFoods = [...foods];
-      if (foods !== []){
-        newFoods.splice(0, 3);
-      };
-      setTotal("");
+      if (check !== number) {
+        setMenus([]);
+        setError("OKタッチ!");
+      } else if (check === number) {
+        const newKcals = [...kcals];
+        if (kcals !== []){
+          newKcals.splice(0, 3);
+        };
+        const newFoods = [...foods];
+        if (foods !== []){
+          newFoods.splice(0, 3);
+        };
+        setTotal("");
 
-      if (number === "1"){
-        newKcals.push("330kcal", "292kcal", "370kcal");
-        newFoods.push("かつ丼（893kcal）", "カルボナーラ（830kcal）", "ビーフカレー（954kcal）");
-        setTotal("992kcal");
-      } else if (number === "2"){
-        newKcals.push("230kcal", "283kcal", "179kcal");
-        newFoods.push("サバみそ定食（687kcal）", "広島焼き（633kcal）", "麻婆豆腐（648kcal）");
-        setTotal("692kcal");
-      } else if (number === "3"){
-        newKcals.push("131kcal", "145kcal", "134kcal");
-        newFoods.push("きつねうどん（382kcal）", "ハンバーガー（300kcal）", "春巻き（369kcal）");
-        setTotal("410kcal");
-      } else if (number === "4"){
-        newKcals.push("180kcal", "105kcal", "92kcal");
-        newFoods.push("チーズバーガー（368kcal）", "チョコケーキ（352kcal）", "クリームあんみつ（353kcal）");
-        setTotal("377kcal");
-      } else if (number === "5"){
-        newKcals.push("61kcal", "108kcal", "43kcal");
-        newFoods.push("肉まん（201kcal）", "シュークリーム（209kcal）", "たいやき（211kcal）");
-        setTotal("212kcal");
-      } else if (number === "6"){
-        newKcals.push("66kcal", "60kcal", "79kcal");
-        newFoods.push("杏仁豆腐（125kcal）", "みたらし団子（118kcal）", "カフェオレ（71kcal）");
-        setTotal("205kcal");
+        if (number === "1"){
+          newKcals.push("330kcal", "292kcal", "370kcal");
+          newFoods.push("かつ丼（893kcal）", "カルボナーラ（830kcal）", "ビーフカレー（954kcal）");
+          setTotal("992kcal");
+        } else if (number === "2"){
+          newKcals.push("230kcal", "283kcal", "179kcal");
+          newFoods.push("サバみそ定食（687kcal）", "広島焼き（633kcal）", "麻婆豆腐（648kcal）");
+          setTotal("692kcal");
+        } else if (number === "3"){
+          newKcals.push("131kcal", "145kcal", "134kcal");
+          newFoods.push("きつねうどん（382kcal）", "ハンバーガー（300kcal）", "春巻き（369kcal）");
+          setTotal("410kcal");
+        } else if (number === "4"){
+          newKcals.push("180kcal", "105kcal", "92kcal");
+          newFoods.push("チーズバーガー（368kcal）", "チョコケーキ（352kcal）", "クリームあんみつ（353kcal）");
+          setTotal("377kcal");
+        } else if (number === "5"){
+          newKcals.push("61kcal", "108kcal", "43kcal");
+          newFoods.push("肉まん（201kcal）", "シュークリーム（209kcal）", "たいやき（211kcal）");
+          setTotal("212kcal");
+        } else if (number === "6"){
+          newKcals.push("66kcal", "60kcal", "79kcal");
+          newFoods.push("杏仁豆腐（125kcal）", "みたらし団子（118kcal）", "カフェオレ（71kcal）");
+          setTotal("205kcal");
+        }
+        setKcals(newKcals);
+        setFoods(newFoods);
       }
-      setKcals(newKcals);
-      setFoods(newFoods);
     }
 
   return (
